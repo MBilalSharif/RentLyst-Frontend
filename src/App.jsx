4afba6dbb2edb2c2437d701decdbad23cdffdee7
MBilalSharif <bilalsaeedmbs@gmail.com>
2025-09-14 02:16:14 +0500

    your message here

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-// import './index.css';
-
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Register from './components/Register';
 import RentingListings from './components/RentingListings';
-
 import LandlordRegister from './components/LandlordRegister';
 import LandlordDashboard from './components/LandlordDashboard';
 import AddRentalProperty from './components/AddRentalProperty';
